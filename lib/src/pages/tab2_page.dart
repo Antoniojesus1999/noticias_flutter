f7fba01fefa6e8421e35b54ecf0b947de464cc22
Antonio Jesús Ponce Vela <antoniojesuspv99@gmail.com>
2022-12-08 17:23:49 +0100

    implementando el menu escrol horizontal de los encabezados

--- lib/src/pages/tab2_page.dart
+++ lib/src/pages/tab2_page.dart
@@ -1,13 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:noticias_flutter/src/models/categroy_model.dart';
+import 'package:provider/provider.dart';
+
+import '../services/news_service.dart';
 
 class Tab2Page extends StatelessWidget {
   const Tab2Page({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('HomeScreen'),
+    return SafeArea(
+        child: Scaffold(
+      body: Column(
+        children: const [
+          Expanded(
+            child: _ListaCategorias(),
+          )
+        ],
+      ),
+    ));
+  }
+}
+
+class _ListaCategorias extends StatelessWidget {
+  const _ListaCategorias({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final categories = Provider.of<NewsService>(context).categories;
+    return ListView.builder(
+      physics: const BouncingScrollPhysics(),
+      scrollDirection: Axis.horizontal,
+      itemCount: categories.length,
+      itemBuilder: (context, index) {
+        final cName = categories[index].name;
+        return Padding(
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            children: [
+              _CategoryButton(
+                categories[index],
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Text('${cName[0].toUpperCase() + cName.substring(1)}')
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _CategoryButton extends StatelessWidget {
+  final Category categoria;
+  const _CategoryButton(
+    this.categoria,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print('${categoria.name}');
+      },
+      child: Container(
+        width: 40,
+        height: 40,
+        margin: EdgeInsets.symmetric(horizontal: 10),
+        decoration:
+            const BoxDecoration(shape: BoxShape.circle, color: Colors.white),
+        child: Icon(categoria.icon, color: Colors.black54),
       ),
     );
   }
--- lib/src/services/news_service.dart
+++ lib/src/services/news_service.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:http/http.dart' as http;
+import 'package:noticias_flutter/src/models/categroy_model.dart';
 import '../models/newResponseModel.dart';
 
 const _URL_NEWS = 'https://newsapi.org/v2';
@@ -7,6 +9,15 @@ const _API_KEY = '23a3bfa396ab4cdb9980f4824ccb5344';
 
 class NewsService with ChangeNotifier {
   List<Article> headlines = [];
+  List<Category> categories = [
+    Category(FontAwesomeIcons.building, 'business'),
+    Category(FontAwesomeIcons.tv, 'entertainment'),
+    Category(FontAwesomeIcons.addressCard, 'general'),
+    Category(FontAwesomeIcons.headSideVirus, 'healt'),
+    Category(FontAwesomeIcons.vials, 'science'),
+    Category(FontAwesomeIcons.volleyball, 'sports'),
+    Category(FontAwesomeIcons.memory, 'technology'),
+  ];
 
   NewsService() {
     getToHeadLines();
